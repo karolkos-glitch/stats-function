a45763a79f77305a482f150d761c3889c9af47d4
Karol <karol99kos@gmail.com>
2023-07-12 17:15:14 +0200

    Add convertion string to float

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import type { Component, JSXElement,  } from 'solid-js';
-import { createSignal, Switch, Match, onMount, createEffect } from 'solid-js';
-
+import type { Component  } from 'solid-js';
+import { createSignal, Switch, Match, createEffect } from 'solid-js';
 
 type APP_STATE = 'PASS_ARGUMENTS' | 'CALCULATING' | 'SEE_RESULTS';
 
@@ -9,7 +8,6 @@ const initialState: APP_STATE = 'PASS_ARGUMENTS';
 const [appState, setAppState] = createSignal<APP_STATE>(initialState);
 
 const App: Component = () => {
-
   const calculating = () => appState() !== 'PASS_ARGUMENTS' ? "step-primary" : "";
   const seeResults = () => appState() === 'SEE_RESULTS' ? "step-primary" : "";
 
@@ -44,29 +42,52 @@ const App: Component = () => {
   );
 };
 
+export type ArgumentsFormFields = {
+  m_01: number;
+  m_02: number;
+  phi_1: number;
+  phi_2: number;
+  phi_3: number;
+};
+
+const [argumentsForm, setArgumentsForm] = createSignal<ArgumentsFormFields>({
+  m_01: 0,
+  m_02: 0,
+  phi_1: 0,
+  phi_2: 0,
+  phi_3: 0
+});
+
+const updateFormField = (formFieldKey: keyof ArgumentsFormFields, value: number) => {
+  setArgumentsForm(prev => ({...prev, [formFieldKey]: value }));
+}
+
 const PassArguments: Component = () => {
-  return <form onSubmit={() => setAppState(() => "CALCULATING")}>
+  return <form onSubmit={() => {
+    setAppState(() => "CALCULATING")
+    console.log(argumentsForm());
+  }}>
     <div class='my-4'>
       <label class="mx-8" for="m_01">M 1</label>
-      <input name="m_01" value={2} type="number" class="input input-bordered input-primary w-full max-w-xs" />
+      <input name="m_01" value={argumentsForm().m_01}  onChange={(e) => updateFormField("m_01", parseFloat(e.currentTarget.value))} type="text" class="input input-bordered input-primary w-full max-w-xs" />
     </div>
     <div class='my-4'>
       <label class="mx-8" for="m_02">M 2</label>
-      <input name="m_02" value={4} type="number" class="input input-bordered input-primary w-full max-w-xs" />
+      <input name="m_02" value={argumentsForm().m_02} onChange={(e) => updateFormField("m_02", parseFloat(e.currentTarget.value))} type="text" class="input input-bordered input-primary w-full max-w-xs" />
     </div>
     <div class='my-4'>
       <label class="mx-8" for="phi_1">PHI 1</label>
-      <input name="phi_1" value={0} type="number" class="input input-bordered input-primary w-full max-w-xs" />
+      <input name="phi_1" value={argumentsForm().phi_1} onChange={(e) => updateFormField("phi_1", parseFloat(e.currentTarget.value))}  type="text" class="input input-bordered input-primary w-full max-w-xs" />
     </div>
     <div class='my-4'>
       <label class="mx-8" for="phi_2">PHI 2</label>
-      <input name="phi_2" value={20} type="number" class="input input-bordered input-primary w-full max-w-xs" />
+      <input name="phi_2" value={argumentsForm().phi_2} onChange={(e) => updateFormField("phi_2", parseFloat(e.currentTarget.value))} type="text" class="input input-bordered input-primary w-full max-w-xs" />
     </div>
     <div class='my-4'>
       <label class="mx-8" for="phi_3">
         PHI 3
       </label>
-       <input name="phi_3" value={30} type="number" class="input input-bordered input-primary w-full max-w-xs" />
+       <input name="phi_3" value={argumentsForm().phi_3} onChange={(e) => updateFormField("phi_3", parseFloat(e.currentTarget.value))} type="text" class="input input-bordered input-primary w-full max-w-xs" />
     </div>
     <button type="submit" class="btn btn-primary">Calculate</button>
   </form>
@@ -106,4 +127,5 @@ const SeeResults: Component = () => {
   
 </div>
 }
+
 export default App;
---
+++ src/math/math.ts
@@ -0,0 +1,8 @@
+import { ArgumentsFormFields } from "../App";
+
+const mainMath = ({
+  m_01,
+  m_02
+}: ArgumentsFormFields) => {
+
+}
